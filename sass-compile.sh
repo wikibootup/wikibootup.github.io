fad79aaeda1084e130eea7482f0794c349cc4860
wikibootup <wikibootup@gmail.com>
2017-04-02 23:58:18 +0900

    modularize & minify css

--- sass-compile.sh
+++ sass-compile.sh
@@ -1,2 +1,3 @@
 # temporary shell code, use a build tool later.
-node-sass static/sass/blog.scss static/css/lanyon.css
+node-sass static/sass/blog.scss static/css/app.css
+csso static/css/app.css static/css/app.min.css
